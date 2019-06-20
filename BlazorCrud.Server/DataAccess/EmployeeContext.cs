934153d0cac0447e59aacafb145e40c47148df38
nar300 <ni781233@gmail.com>
2019-06-20 23:19:16 +0100

    first commit

---
+++ BlazorCrud.Server/DataAccess/EmployeeContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorCrud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.DataAccess
+{
+  
+        public class EmployeeContext : DbContext
+        {
+            public virtual DbSet<Employee> tblEmployee { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer(@"server=LOTUS\SQLEXPRESS;database=EmployeeNDB;integrated security=true;");
+                }
+            }
+        }
+    }
+
